b944046c7372ccf883fdbe9e9194a0c029a05148
amsh-777 <139392048+amsh-777@users.noreply.github.com>
2024-09-13 14:00:40 -0600

    week 2 assignment complete
    i completed all the given tasks

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <h2>Aman Sharma</h2>
+            <Link href="https://github.com/amsh-777">https://github.com</Link>
+        </div>
+
+    );
+}
